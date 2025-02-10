f0d387558dcc5f2f81e4f656dbcccd35ac0bdbf9
Alessandro-Marabese <alessandro.marabese93@gmail.com>
2025-02-11 00:36:33 +0100

    commit

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,33 @@
+import { Button, ListGroup } from "react-bootstrap";
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
+        method: "DELETE",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNjdlY2I3NDcwMTAwMTU4YjJiNTkiLCJpYXQiOjE3MzkyMDM3NzAsImV4cCI6MTc0MDQxMzM3MH0.rpZpgnYKS_fJmbYRYWaXK-MYiS0LfBlir4wnkU8RWV8",
+        },
+      });
+      if (response.ok) {
+        alert("La recensione è stata elimata!");
+      } else {
+        throw new Error("La recensione non è stata eliminata!");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <ListGroup.Item>
+      {comment.comment}
+      <Button variant="danger" className="ms-2" onClick={() => deleteComment(comment._id)}>
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  );
+};
+
+export default SingleComment;
