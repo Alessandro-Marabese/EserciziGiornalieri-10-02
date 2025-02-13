39f8c4c46e731abd5461d3f3cf1c91d6edf21525
Alessandro-Marabese <alessandro.marabese93@gmail.com>
2025-02-13 16:15:03 +0100

    commit

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,46 +1,39 @@
-import { Component } from "react";
+import { useState } from "react";
 import SingleBook from "./SingleBook";
 import { Col, Form, Row } from "react-bootstrap";
 import CommentArea from "./CommentArea";
 
-class BookList extends Component {
-  state = {
-    searchQuery: "",
-    asin: "",
+const BookList = ({ books }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [asin, setAsin] = useState("");
+  const bookSelection = (asin) => {
+    setAsin(asin);
   };
-  bookSelection = (bookAsin) => this.setState({ asin: bookAsin });
-  render() {
-    return (
-      <>
-        <Row className="justify-content-center mt-5">
-          <Col xs={12} md={4} className="text-center">
-            <Form.Group>
-              <Form.Control
-                type="search"
-                placeholder="Cerca un libro"
-                value={this.state.searchQuery}
-                onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="col-9">
-            <Row className="g-2 mt-3">
-              {this.props.books
-                .filter((b) => b.title.toLowerCase().includes(this.state.searchQuery))
-                .map((b) => (
-                  <Col xs={12} md={4} key={b.asin}>
-                    <SingleBook book={b} bookSelection={this.bookSelection} asin={this.state.asin} />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          <Col className="col-3">{this.state.asin !== "" && <CommentArea asin={this.state.asin} />}</Col>
-        </Row>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} md={4} className="text-center">
+          <Form.Group>
+            <Form.Control type="search" placeholder="Cerca un libro" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="col-9">
+          <Row className="g-2 mt-3">
+            {books
+              .filter((b) => b.title.toLowerCase().includes(searchQuery))
+              .map((b) => (
+                <Col xs={12} md={4} key={b.asin}>
+                  <SingleBook book={b} bookSelection={bookSelection} asin={asin} />
+                </Col>
+              ))}
+          </Row>
+        </Col>
+        <Col className="col-3">{asin !== "" && <CommentArea asin={asin} />}</Col>
+      </Row>
+    </>
+  );
+};
 
 export default BookList;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,39 +1,21 @@
-import { Component } from "react";
 import { Card } from "react-bootstrap";
 
-class SingleBook extends Component {
-  state = {
-    selected: false,
-    asin: "",
-  };
-  changeAsin = () => {
-    if (this.state.asin === "") {
-      this.setState({ asin: this.props.book.asin });
-    } else if (this.state.asin !== "") {
-      this.setState({ asin: "" });
-    }
-  };
-  render() {
-    return (
-      <>
-        <Card
-          onClick={() => {
-            this.setState({
-              selected: !this.state.selected,
-            });
-            this.props.bookSelection(this.props.book.asin);
-            this.changeAsin();
-          }}
-          style={{ border: this.state.asin === this.props.book.asin ? "3px solid red" : "none" }}
-        >
-          <Card.Img variant="top" src={this.props.book.img} />
-          <Card.Body>
-            <Card.Title style={{ color: "black" }}>{this.props.book.title}</Card.Title>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  }
-}
+const SingleBook = ({ bookSelection, asin, book }) => {
+  return (
+    <>
+      <Card
+        onClick={() => {
+          bookSelection(book.asin);
+        }}
+        style={{ border: asin === book.asin ? "3px solid red" : "none" }}
+      >
+        <Card.Img variant="top" src={book.img} />
+        <Card.Body>
+          <Card.Title style={{ color: "black" }}>{book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default SingleBook;
